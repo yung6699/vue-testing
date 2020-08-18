8f05ae9a2468f94e8091c4fc8493cb1596135e35
Andy <yty3592@gmail.com>
2020-08-18 18:38:28 +0900

    FormSubmitter 컴포넌트 테스트 코드 작성중

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -2,8 +2,12 @@ import { shallowMount } from '@vue/test-utils';
 import FormSubmitter from '@/components/FormSubmitter.vue';
 
 describe('FormSubmitter.vue', () => {
-  it('제출했을 때 알림이 나타난다.', () => {
+  it('제출했을 때 알림이 나타난다.', async () => {
     const wrapper = shallowMount(FormSubmitter);
-    wrapper.find("[data-username]")
+    wrapper.find("[data-username]").setValue("alice");
+    wrapper.find("form").trigger("submit.prevent");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find(".message").text())
+      .toBe("Thank you for your submission, alice.");
   })
 })
