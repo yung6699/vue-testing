a4be17fcf9089f857e414f6c8e4b3fb5570c2667
Andy <yty3592@gmail.com>
2020-09-03 17:16:53 +0900

    컴포넌트 lazy-loading 테스트 코드 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <ChildComponent/>
+    <button v-on:click="onClick">Toggle Child Component!</button>
+    <GreetingComponent v-if="isShow"></GreetingComponent>
   </div>
 </template>
 
 <script>
+
+import ChildComponent from '@/components/Child.vue';
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    ChildComponent,
+    GreetingComponent: null
+  },
+
+  data: () => ({
+    isShow: false
+  }),
+
+  mounted() {
+    console.log('start', this.$options.components['GreetingComponent'])
+    setTimeout(() => {
+      this.$options.components['GreetingComponent'] = lazyLoadComponent({
+        componentFactory: import('@/components/Greeting.vue')
+      })
+      console.log('end', this.$options.components['GreetingComponent'])
+    }, 5000)
+  },
+
+  methods: {
+    onClick () {
+      this.isShow = !this.isShow;
+    }
+  }
+}
+
+function lazyLoadComponent ({ componentFactory }) {
+  return () => ({
+    component: componentFactory
+  })
 }
 </script>
